3064dd7e7643f4a986a669464c8681f8c66ef5c2
Salem <salem.ribeiro@sapo.pt>
2021-01-24 02:50:04 -0300

    new functions for normalization and check

--- main.c
+++ main.c
@@ -5,7 +5,9 @@ int main(int argc, char *argv[])
     if (argc == 1 || argc > 3)
         message_err(ARGUMENT);
     read_file(argv[1]);
-	clear_pointer(g_map);
+	normalize_map(g_map);
+	check_map(g_world.map);
+	clear_pointer(g_world.map);
 	clear_header();
     return(0);
 }
